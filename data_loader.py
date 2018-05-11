56907ffd0d5c2b7ec697650b8c9e93b719ab7345
Ki Hyun Kim <pointzz.ki@gmail.com>
2018-05-11 12:51:19 +0900

    make pretty

--- data_loader.py
+++ data_loader.py
@@ -49,11 +49,26 @@ class TextClassificationDataLoader():
         super(TextClassificationDataLoader, self).__init__()
 
         self.label = data.Field(sequential = False, use_vocab = False)
-        self.text = data.Field(tokenize = tokenizer, use_vocab = True, batch_first = True, include_lengths = True, fix_length = fix_length, eos_token = '<EOS>' if use_eos else None)
+        self.text = data.Field(tokenize = tokenizer, 
+                                use_vocab = True, 
+                                batch_first = True, 
+                                include_lengths = True, 
+                                fix_length = fix_length, 
+                                eos_token = '<EOS>' if use_eos else None
+                                )
 
-        train, valid = data.TabularDataset.splits(path = '', train = train_fn, validation = valid_fn, format = 'tsv', fields = [('label', self.label), ('text', self.text)])
+        train, valid = data.TabularDataset.splits(path = '', 
+                                                    train = train_fn, 
+                                                    validation = valid_fn, 
+                                                    format = 'tsv', 
+                                                    fields = [('label', self.label), ('text', self.text)]
+                                                    )
 
-        self.train_iter, self.valid_iter = data.BucketIterator.splits((train, valid), batch_size = batch_size, device = device, shuffle = shuffle)
+        self.train_iter, self.valid_iter = data.BucketIterator.splits((train, valid), 
+                                                                        batch_size = batch_size, 
+                                                                        device = device, 
+                                                                        shuffle = shuffle
+                                                                        )
         
         self.label.build_vocab(train)
         self.text.build_vocab(train, max_size = max_vocab)
